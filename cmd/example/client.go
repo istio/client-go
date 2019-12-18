d7c923efe68aefb2e00459f78ee7590debf7559d
Travis Clarke <clarketm@google.com>
2019-12-17 19:26:07 -0800

    Update common files

--- cmd/example/client.go
+++ cmd/example/client.go
@@ -30,9 +30,11 @@ import (
 func main() {
 	kubeconfig := os.Getenv("KUBECONFIG")
 	namespace := os.Getenv("NAMESPACE")
+
 	if len(kubeconfig) == 0 || len(namespace) == 0 {
 		log.Fatalf("Environment variables KUBECONFIG and NAMESPACE need to be set")
 	}
+
 	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		log.Fatalf("Failed to create k8s rest client: %s", err)
@@ -42,11 +44,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to create istio client: %s", err)
 	}
+
 	// Test VirtualServices
 	vsList, err := ic.NetworkingV1alpha3().VirtualServices(namespace).List(metav1.ListOptions{})
 	if err != nil {
 		log.Fatalf("Failed to get VirtualService in %s namespace: %s", namespace, err)
 	}
+
 	for i := range vsList.Items {
 		vs := vsList.Items[i]
 		log.Printf("Index: %d VirtualService Hosts: %+v\n", i, vs.Spec.GetHosts())
@@ -57,6 +61,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to get DestinationRule in %s namespace: %s", namespace, err)
 	}
+
 	for i := range drList.Items {
 		dr := drList.Items[i]
 		log.Printf("Index: %d DestinationRule Host: %+v\n", i, dr.Spec.GetHost())
@@ -67,6 +72,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to get Policy in %s namespace: %s", namespace, err)
 	}
+
 	for i := range pList.Items {
 		p := pList.Items[i]
 		log.Printf("Index: %d Policy Targets: %+v\n", i, p.Spec.GetTargets())
@@ -77,12 +83,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to list MeshPolicies", err)
 	}
+
 	for i := range mpList.Items {
 		mp := mpList.Items[i]
 		log.Printf("Index: %d MeshPolicy Name: %+v\n", i, mp.ObjectMeta.Name)
 
 		// Known broken without the custom marshal/unmarshal code
 		log.Printf("Index %d MeshPolicy Value: %+v\n", i, mp.Spec.Peers)
+
 		_, err := ic.AuthenticationV1alpha1().MeshPolicies().Get(mp.ObjectMeta.Name, metav1.GetOptions{})
 		if err != nil {
 			log.Fatalf("Failed to get MeshPolicy named %s", mp.ObjectMeta.Name)
@@ -94,6 +102,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to get Gateway in %s namespace: %s", namespace, err)
 	}
+
 	for i := range gwList.Items {
 		gw := gwList.Items[i]
 		for _, s := range gw.Spec.GetServers() {
@@ -106,6 +115,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to get ServiceEntry in %s namespace: %s", namespace, err)
 	}
+
 	for i := range seList.Items {
 		se := seList.Items[i]
 		for _, h := range se.Spec.GetHosts() {
