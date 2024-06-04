16a593e403966bcaa1a30cd1a6c4aa43a1384bc7
istio-testing <istio-testing-bot@google.com>
2024-06-04 21:17:15 +0000

    Automator: update istio/api@master dependency in istio/client-go@master

--- pkg/apis/security/v1beta1/types.gen.go
+++ pkg/apis/security/v1beta1/types.gen.go
@@ -200,6 +200,7 @@ type AuthorizationPolicyList struct {
 // +genclient
 // +k8s:deepcopy-gen=true
 // -->
+// +kubebuilder:validation:XValidation:message="portLevelMtls requires selector",rule="(has(self.selector) && has(self.selector.matchLabels) && self.selector.matchLabels.size() > 0) || !has(self.portLevelMtls)"
 type PeerAuthentication struct {
 	v1.TypeMeta `json:",inline"`
 	// +optional
@@ -482,6 +483,7 @@ type PeerAuthenticationList struct {
 // +genclient
 // +k8s:deepcopy-gen=true
 // -->
+// +kubebuilder:validation:XValidation:message="only one of targetRefs or workloadSelector can be set",rule="(has(self.selector)?1:0)+(has(self.targetRef)?1:0)+(has(self.targetRefs)?1:0)<=1"
 type RequestAuthentication struct {
 	v1.TypeMeta `json:",inline"`
 	// +optional
