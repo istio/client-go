3354a2278d2a81d0d63fef017099944e05d464ed
Istio Automation <istio-testing-bot@google.com>
2025-09-22 11:13:12 -0700

    Automator: update istio/api@master dependency in istio/client-go@master (#2540)

--- pkg/apis/security/v1/types.gen.go
+++ pkg/apis/security/v1/types.gen.go
@@ -29,7 +29,7 @@ import (
 //
 // <!-- crd generation tags
 // +cue-gen:AuthorizationPolicy:groupName:security.istio.io
-// +cue-gen:AuthorizationPolicy:versions:v1beta1,v1
+// +cue-gen:AuthorizationPolicy:versions:v1,v1beta1
 // +cue-gen:AuthorizationPolicy:storageVersion
 // +cue-gen:AuthorizationPolicy:annotations:helm.sh/resource-policy=keep
 // +cue-gen:AuthorizationPolicy:labels:app=istio-pilot,chart=istio,istio=security,heritage=Tiller,release=istio
@@ -78,7 +78,7 @@ type AuthorizationPolicyList struct {
 
 // <!-- crd generation tags
 // +cue-gen:PeerAuthentication:groupName:security.istio.io
-// +cue-gen:PeerAuthentication:versions:v1beta1,v1
+// +cue-gen:PeerAuthentication:versions:v1,v1beta1
 // +cue-gen:PeerAuthentication:storageVersion
 // +cue-gen:PeerAuthentication:annotations:helm.sh/resource-policy=keep
 // +cue-gen:PeerAuthentication:labels:app=istio-pilot,chart=istio,istio=security,heritage=Tiller,release=istio
@@ -127,7 +127,7 @@ type PeerAuthenticationList struct {
 
 // <!-- crd generation tags
 // +cue-gen:RequestAuthentication:groupName:security.istio.io
-// +cue-gen:RequestAuthentication:versions:v1beta1,v1
+// +cue-gen:RequestAuthentication:versions:v1,v1beta1
 // +cue-gen:RequestAuthentication:storageVersion
 // +cue-gen:RequestAuthentication:annotations:helm.sh/resource-policy=keep
 // +cue-gen:RequestAuthentication:labels:app=istio-pilot,chart=istio,istio=security,heritage=Tiller,release=istio
